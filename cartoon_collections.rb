a2f9e1d3427bacb256cfc401abccf06aa4d2b81a
Louie O'Grady <l.ogrady@gmail.com>
2019-02-05 14:05:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.select {|n| n.length > 4}
+  array.select {|n| n > 4}
 end
 
 def find_the_cheese# code an argument here
