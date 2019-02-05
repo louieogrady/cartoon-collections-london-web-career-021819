f6134a614bb208f333dbaa837b71aa8c93463291
Louie O'Grady <l.ogrady@gmail.com>
2019-02-05 14:08:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,9 @@ def long_planeteer_calls(array)
   array.any?{|n| n.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  array.find("cheese")
+
+
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
