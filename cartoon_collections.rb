7a92466c44bc561fd37ba938487b585ae806fd9d
Louie O'Grady <l.ogrady@gmail.com>
2019-02-05 13:51:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |id, index| 
+    puts "#{index+1} #{id}"
 end
+end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
